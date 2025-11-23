72718ad1e3a11f6a581c078c728561b7aebb8b57
KlafaKEN <klllafaken@gmail.com>
2025-11-23 13:25:47 +0100

    v0.1

---
+++ LookDesk-frontend/src/components/Sidebar.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+    <aside class="sidebar">
+        <div class="sidebar__inner">
+            <div class="sidebar__search">
+                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11.6666 11.6665L14.6666 14.6665" stroke="#7A7482" stroke-linecap="round" stroke-linejoin="round"/><path d="M13.3334 7.3335C13.3334 4.01979 10.6471 1.3335 7.33337 1.3335C4.01967 1.3335 1.33337 4.01979 1.33337 7.3335C1.33337 10.6472 4.01967 13.3335 7.33337 13.3335C10.6471 13.3335 13.3334 10.6472 13.3334 7.3335Z" stroke="#7A7482" stroke-linejoin="round"/></svg>
+                 <input type="text" placeholder="Search policies, procedures">                   
+            </div>
+            <div class="sidebar__categories">
+                <h3>Categories</h3>
+                <button></button>
+                <button></button>
+                <button></button>
+            </div>
+            <div class="sidebar__actions">
+                <h3>Quick Actions</h3>
+                <button></button>
+                <button></button>
+            </div>
+        </div>
+    </aside>
+</template>
---
+++ LookDesk-frontend/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import Header from '../components/Header.vue';
+    import Sidebar from '../components/Sidebar.vue';
+</script>
+
+<template>
+    <div class="wrapper">
+        <Header></Header>
+        <main class="main">
+            <div class="container">
+                <Sidebar></Sidebar>
+            </div>
+        </main>
+    </div>
+</template>
